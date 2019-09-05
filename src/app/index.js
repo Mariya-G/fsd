367844a046160fee0c0268aabc3e978b4c300dce
Mariya Grigoreva <mariegrigoriiva@gmail.com>
2019-09-05 21:38:57 +0300

    add block header with pilot logo

---
+++ src/app/index.js
@@ -0,0 +1,6 @@
+// точка входа в проект
+import '../app/app.scss'
+
+jQuery(document).ready(function($) {
+    $('body').css('background-color', 'gray');
+}) 
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+// basic vars
+const path = require('path')
+const webpack = require('webpack')
+
+//additional plugins
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+//const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const PAGES_DIR = '${PATHS.SRC}/pug/pages'
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+//module settings
+// обращение к объекту Модуль:
+module.exports = {
+  // точки входа js
+  entry: './src/app/index.js',
+  // точки выхода
+  output: {
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+    devServer: {
+      contentBase: './src/public',
+      compress: true,
+      port: 9000
+  },
+  module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.pug$/,
+          use: 'pug-loader'
+          }
+        // /* {
+        //   test: /\.css$/,
+        //   use: [
+        //     {
+        //       loader: MiniCssExtractPlugin.loader,
+        //     },
+        //     'css-loader'
+        //   ]
+
+        // } */
+      ]
+        },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/blocks/header/header.pug')
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    })
+  ]
+} 
